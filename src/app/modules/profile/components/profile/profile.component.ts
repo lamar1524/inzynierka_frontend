f2a267b1710b31dd5d1e9f68b4bebfed3d79fcd5
lamar1524 <pawel.wybrzeze@gmail.com>
2020-07-18 13:43:03 +0200

    Added editing profile logic

--- src/app/modules/profile/components/profile/profile.component.ts
+++ src/app/modules/profile/components/profile/profile.component.ts
@@ -1,9 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { selectCurrentUser, AuthModuleState } from '@authorization/store';
 import { IUser } from '@core/interfaces';
+import { equalityValidator } from '@core/validators/equality.validator';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
 import { selectProfileData, selectProfileLoading, ProfileModuleState } from '../../store';
 import * as profileActions from '../../store/profile.actions';
 
@@ -14,23 +17,77 @@ import * as profileActions from '../../store/profile.actions';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent implements OnDestroy {
-  currentUser$: Observable<IUser>;
-  profile$: Observable<IUser>;
+  profile: IUser;
   profileLoading$: Observable<boolean>;
+  currentUser: IUser;
   sub$: Subscription;
+  editing: boolean;
+  editForm: FormGroup;
 
-  constructor(private store: Store<AuthModuleState | ProfileModuleState>, private route: ActivatedRoute) {
+  constructor(private store: Store<AuthModuleState | ProfileModuleState>, private route: ActivatedRoute, private cdRef: ChangeDetectorRef) {
+    this._initValues();
+    this._initSubscriptions();
+  }
+
+  get isCurrentUser(): boolean {
+    return this.currentUser.id === this.profile.id;
+  }
+
+  private _initValues(): void {
     this.sub$ = new Subscription();
+    this.editing = false;
+  }
+
+  private _initSubscriptions(): void {
+    const currentUser$ = this.store
+      .select(selectCurrentUser)
+      .pipe(
+        filter((user) => {
+          return !!user;
+        }),
+      )
+      .subscribe((user) => {
+        this.currentUser = user;
+        this.cdRef.markForCheck();
+      });
+    this.sub$.add(currentUser$);
+
+    const profile$ = this.store
+      .select(selectProfileData)
+      .pipe(filter((profile) => profile !== null))
+      .subscribe((profile) => {
+        this.profile = profile;
+        this._initForm();
+      });
+    this.sub$.add(profile$);
+
     const route$ = this.route.params.subscribe((params) => {
       if (params.id.length > 0) {
         this.store.dispatch(profileActions.loadProfileData({ userId: params.id }));
       }
     });
     this.sub$.add(route$);
-    this.currentUser$ = this.store.select(selectCurrentUser);
-    this.profile$ = this.store.select(selectProfileData);
+
     this.profileLoading$ = this.store.select(selectProfileLoading);
   }
+
+  // TODO - password + repeat password fields
+  private _initForm(): void {
+    this.editForm = new FormGroup({
+      firstName: new FormControl(this.profile.first_name, [Validators.required]),
+      lastName: new FormControl(this.profile.last_name, [Validators.required]),
+      email: new FormControl(this.profile.email, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      repeatPassword: new FormControl(null, [Validators.required, equalityValidator('password')]),
+    });
+  }
+
+  editToggle(): void {
+    this.editing = !this.editing;
+  }
+
+  // TODO: Method for sending data
+  submitUpdate(): void {}
 
   ngOnDestroy(): void {
     this.sub$.unsubscribe();
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
@@ -15,6 +19,10 @@ import { profileReducer, ProfileEffects } from './store';
     ProfileRoutingModule,
     StoreModule.forFeature(STORE_FEATURES.profile, profileReducer),
     EffectsModule.forFeature([ProfileEffects]),
+    MatButtonModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule,
   ],
 })
 export class ProfileModule {}
