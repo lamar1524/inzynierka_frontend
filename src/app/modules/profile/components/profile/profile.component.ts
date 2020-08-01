7a80d44bc623648f0513377a0b09f5062aa3bc78
lamar1524 <pawel.wybrzeze@gmail.com>
2020-08-01 12:05:02 +0200

    Template for editing profile details

--- src/app/modules/profile/components/profile/profile.component.ts
+++ src/app/modules/profile/components/profile/profile.component.ts
@@ -6,7 +6,7 @@ import { IUser } from '@core/interfaces';
 import { equalityValidator } from '@core/validators/equality.validator';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
-import { filter, tap } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 import { selectProfileData, selectProfileLoading, ProfileModuleState } from '../../store';
 import * as profileActions from '../../store/profile.actions';
 
@@ -23,14 +23,16 @@ export class ProfileComponent implements OnDestroy {
   sub$: Subscription;
   editing: boolean;
   editForm: FormGroup;
+  editPasswordForm: FormGroup;
 
   constructor(private store: Store<AuthModuleState | ProfileModuleState>, private route: ActivatedRoute, private cdRef: ChangeDetectorRef) {
     this._initValues();
     this._initSubscriptions();
+    this._initPasswordForm();
   }
 
   get isCurrentUser(): boolean {
-    return this.currentUser.id === this.profile.id;
+    return !!this.currentUser && !!this.profile && this.currentUser.id === this.profile.id;
   }
 
   private _initValues(): void {
@@ -71,12 +73,16 @@ export class ProfileComponent implements OnDestroy {
     this.profileLoading$ = this.store.select(selectProfileLoading);
   }
 
-  // TODO - password + repeat password fields
   private _initForm(): void {
     this.editForm = new FormGroup({
       firstName: new FormControl(this.profile.first_name, [Validators.required]),
       lastName: new FormControl(this.profile.last_name, [Validators.required]),
       email: new FormControl(this.profile.email, [Validators.required, Validators.email]),
+    });
+  }
+
+  private _initPasswordForm(): void {
+    this.editPasswordForm = new FormGroup({
       password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
       repeatPassword: new FormControl(null, [Validators.required, equalityValidator('password')]),
     });
@@ -86,8 +92,9 @@ export class ProfileComponent implements OnDestroy {
     this.editing = !this.editing;
   }
 
-  // TODO: Method for sending data
-  submitUpdate(): void {}
+  submitUpdate(): void {
+    console.log(this.editForm.value);
+  }
 
   ngOnDestroy(): void {
     this.sub$.unsubscribe();
