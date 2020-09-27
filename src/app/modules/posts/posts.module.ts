a0baf7a7d62e54de3f76db262474ae667ca54107
Paweł <pawel.wybrzeze@gmail.com>
2020-09-27 14:26:30 +0200

    Changed dropdown in comments

--- src/app/modules/posts/posts.module.ts
+++ src/app/modules/posts/posts.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
@@ -33,6 +35,8 @@ import { postsReducer, PostsEffects } from './store';
     MatProgressSpinnerModule,
     NgScrollbarModule,
     InfiniteScrollModule,
+    MatIconModule,
+    MatMenuModule,
   ],
 })
 export class PostsModule {}
